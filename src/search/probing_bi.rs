75d06e82a03f72c1c0793846b9a9629beef61bd9
Johannes <johannes.preisinger@gmail.com>
2023-04-12 21:04:49 +0200

    version 0.7.3: probing bi idiomatic refactoring

--- src/search/probing_bi.rs
+++ src/search/probing_bi.rs
@@ -12,31 +12,25 @@ pub(crate) fn probe_grid(start_coord: (usize, usize), target_coord: (usize, usiz
     let start = grid.node_id(start_coord);
     let target = grid.node_id(target_coord);
 
-    let start_queue = &mut VecDeque::from([Waypoint::from(
-        None, start, None)]);
-    let target_queue = &mut VecDeque::from([Waypoint::from(
-        None, target, None)]);
+    let start_queue = &mut VecDeque::from([Waypoint::from(None, start, None)]);
+    let target_queue = &mut VecDeque::from([Waypoint::from(None, target, None)]);
 
     let mut start_vis: HashMap<usize, Waypoint> = HashMap::new();
     let mut target_vis: HashMap<usize, Waypoint> = HashMap::new();
 
     while !start_queue.is_empty() || !target_queue.is_empty() {
-        let start_result = process_dequeue(start_queue, grid, dirs,
-                                           &mut start_vis, &mut target_vis, target);
-
-        if start_result.is_some() {
-            return start_result.unwrap();
+        if let Some(start_result) = process_dequeue(start_queue, grid, dirs, &mut start_vis,
+                                                    &mut target_vis, target) {
+            return start_result;
         }
 
-        let end_result = process_dequeue(target_queue, grid, dirs,
-                                         &mut target_vis, &mut start_vis, start);
-
-        if end_result.is_some() {
-            return end_result.unwrap();
+        if let Some(end_result) = process_dequeue(target_queue, grid, dirs, &mut target_vis,
+                                                  &mut start_vis, start) {
+            return end_result;
         }
     }
 
-    return Graph::from(Vec::new());
+    Graph::from(Vec::new())
 }
 
 fn process_dequeue(
@@ -47,9 +41,8 @@ fn process_dequeue(
     other_visited: &mut HashMap<usize, Waypoint>,
     target: usize,
 ) -> Option<Graph> {
-    if !deque.is_empty() {
-        let current = dequeue(deque).unwrap();
-        let current_id = (&current).node_id;
+    if let Some(current) = deque.pop_front() {
+        let current_id = current.node_id;
         visited.insert(current_id, current.clone());
 
         if current_id == target {
@@ -58,66 +51,53 @@ fn process_dequeue(
 
         if other_visited.contains_key(&current_id) {
             let mut from_current = path::walk_back(current);
-            let other_edges = other_visited.get(&current_id)
+            let other_edges = other_visited
+                .get(&current_id)
                 .map_or_else(|| Vec::new(), |w| path::walk_back(w.clone()));
             from_current.extend(other_edges);
-            return Some(Graph::from(from_current.clone()));
+            return Some(Graph::from(from_current));
         }
 
-        let result = go_directions(deque, current, grid, directions, &visited, target);
-
-        if result.is_some() {
-            return result;
+        if let Some(result) = go_directions(deque, current, grid, directions, &visited, target) {
+            return Some(result);
         }
     }
 
-    return None;
+    None
 }
 
 pub(crate) fn probe_graph(start: Node, target: Node, graph: &Graph) -> Graph {
-    let start_queue = &mut VecDeque::from([Waypoint::from(
-        None, start.id, None)]);
-    let target_queue = &mut VecDeque::from([Waypoint::from(
-        None, target.id, None)]);
+    let start_queue = &mut VecDeque::from([Waypoint::from(None, start.id, None)]);
+    let target_queue = &mut VecDeque::from([Waypoint::from(None, target.id, None)]);
 
     let mut start_visited: HashMap<usize, Waypoint> = HashMap::new();
     let mut target_visited: HashMap<usize, Waypoint> = HashMap::new();
 
     while !start_queue.is_empty() || !target_queue.is_empty() {
-        let result_start = process_node(start_queue, &mut start_visited,
-                                        &mut target_visited, &target, graph);
-
-        if result_start.is_some() {
-            return Graph::from(result_start.unwrap());
+        if let Some(result_start) = process_node(start_queue, &mut start_visited,
+                                                 &mut target_visited, &target, graph) {
+            return Graph::from(result_start);
         }
 
-        let result_target = process_node(target_queue, &mut target_visited,
-                                         &mut start_visited, &start, graph);
-
-        if result_target.is_some() {
-            return Graph::from(result_target.unwrap());
+        if let Some(result_target) = process_node(target_queue, &mut target_visited,
+                                                  &mut start_visited, &start, graph) {
+            return Graph::from(result_target);
         }
     }
 
-    return Graph::from(Vec::new());
+    Graph::from(Vec::new())
 }
 
-fn process_node(queue: &mut VecDeque<Waypoint>,
-                visited: &mut HashMap<usize, Waypoint>,
+fn process_node(queue: &mut VecDeque<Waypoint>, visited: &mut HashMap<usize, Waypoint>,
                 end_visited: &mut HashMap<usize, Waypoint>,
-                end: &Node,
-                graph: &Graph) -> Option<Vec<Edge>> {
-    if !queue.is_empty() {
-        let current = dequeue(queue).unwrap();
-
+                end: &Node, graph: &Graph, ) -> Option<Vec<Edge>> {
+    if let Some(current) = queue.pop_front() {
         let result = process_edges(queue, &current, end.id, graph, &visited, &end_visited);
-
-        visited.insert(current.node_id, current.clone());
-
-        return result;
+        visited.insert(current.node_id, current);
+        result
+    } else {
+        None
     }
-
-    return None;
 }
 
 fn process_edges(
@@ -153,5 +133,5 @@ fn process_edges(
         }
     }
 
-    return None;
+    None;
 }
